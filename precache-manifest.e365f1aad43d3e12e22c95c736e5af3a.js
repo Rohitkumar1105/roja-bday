564d3d6fc7b3d0f491223c7ae478c3b1e4d8aea0
Rohitkumar1105 <rohit.kv11@gmail.com>
2020-07-04 18:54:13 +0530

    Updates

---
+++ precache-manifest.e365f1aad43d3e12e22c95c736e5af3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f37afa7a6886d86360b71f7c62772c",
+    "url": "/roja-bday/index.html"
+  },
+  {
+    "revision": "52ebb162b2d1b346dce0",
+    "url": "/roja-bday/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "99fe5b2f3e473121e922",
+    "url": "/roja-bday/static/css/main.281cb7d4.chunk.css"
+  },
+  {
+    "revision": "52ebb162b2d1b346dce0",
+    "url": "/roja-bday/static/js/2.f010ad7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/roja-bday/static/js/2.f010ad7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99fe5b2f3e473121e922",
+    "url": "/roja-bday/static/js/main.2bba4cda.chunk.js"
+  },
+  {
+    "revision": "3b8565f32b33ae21b505",
+    "url": "/roja-bday/static/js/runtime-main.d36c28e9.js"
+  },
+  {
+    "revision": "ce0105b42e43a87177168df7c05b9b5a",
+    "url": "/roja-bday/static/media/byju1.ce0105b4.png"
+  },
+  {
+    "revision": "9a2ae0628fcbf76c1b52a7ddc66c01e2",
+    "url": "/roja-bday/static/media/byju2.9a2ae062.png"
+  },
+  {
+    "revision": "3c8ad82306c0f92b9a30b52830bfea35",
+    "url": "/roja-bday/static/media/byju3.3c8ad823.png"
+  }
+]);
